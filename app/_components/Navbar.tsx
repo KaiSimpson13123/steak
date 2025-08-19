1c19829ddd255130eab44fac47dcdc49c405df99
Kai Simpson <kaiysimpson@gmail.com>
2025-08-19 19:29:14 +0800

    Styling

--- app/_components/Navbar.tsx
+++ app/_components/Navbar.tsx
@@ -39,7 +39,7 @@ export default function Navbar() {
   const balanceInUSD = balance ? balance * 0.000001 : 0;
 
   return (
-    <nav className="top-0 left-0 right-0 z-50 backdrop-blur-lg bg-black/40 border-b border-white/10">
+    <nav className="top-0 left-0 right-0 z-50 backdrop-blur-lg bg-[#191b36] border-b border-white/10">
       <div className="max-w-7xl mx-auto px-4 sm:px-6 lg:px-8">
         <div className="flex justify-between items-center h-16 sm:h-20">
           <Link
