f9ad46979ab19a45c6af995425b2a7c71debb0ee
Kai Simpson <kaiysimpson@gmail.com>
2025-08-22 11:19:04 +0800

    Close Limbo

--- app/(games)/limbo/page.tsx
+++ app/(games)/limbo/page.tsx
@@ -6,7 +6,7 @@ export default function Dice() {
     <main className="flex flex-col h-full">
       <div className="flex flex-col lg:flex-row w-full p-4 lg:p-8 flex-1">
         <div className="flex justify-center items-center w-full p-4">
-          <h1>Game Under Work</h1> {/* <LimboContainer /> */}
+          <h1 className="text-white">Game Under Work</h1> {/* <LimboContainer /> */}
         </div>
       </div>
     </main>
