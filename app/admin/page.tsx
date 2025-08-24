7392e24d60279d41500060d0379d95171618fcdd
Kai Simpson <kaiysimpson@gmail.com>
2025-08-24 16:54:36 +0800

    Admin logs

--- app/admin/page.tsx
+++ app/admin/page.tsx
@@ -1,5 +1,7 @@
 "use client";
 
+{/* fuck off isaac */}
+
 import { useAuth } from "@/components/AuthProvider";
 import { useRouter } from "next/navigation";
 import { useEffect, useMemo, useState } from "react";
